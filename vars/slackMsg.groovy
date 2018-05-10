6ec4550560295e13b444bbd140b3bd0ab259ea84
Graham Bevan <gbevan@csc.com>
2018-05-10 11:57:19 +0100

    prevent duplicate messages when slack suppressed

--- vars/slackMsg.groovy
+++ vars/slackMsg.groovy
@@ -5,7 +5,7 @@ def call(Map parameters) {
   def status = parameters.get('status', 'STARTED')
   def msg = parameters.get('msg', '')
   def branch = scm.branches[0].name
-  println("slackMsg status ${status}, msg ${msg}")
+  // println("slackMsg status ${status}, msg ${msg}")
 
   def color = 'danger'
   if (status =~ /^STARTED/) {
@@ -20,7 +20,7 @@ def call(Map parameters) {
   def combinedMsg = "${status}: Job ${currentBuild.fullDisplayName} branch: ${branch}: ${msg}"
   if (env.SLACK_SUPPRESS != "1")
     slackSend(color: color, message: combinedMsg)
-  else {
-    echo combinedMsg
+  // else {
   }
+  echo combinedMsg
 }
